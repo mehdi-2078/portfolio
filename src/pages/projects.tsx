083422ebcd46b25f30142e221ad21a29bac101ff
faezehJafari <faezehj487@gmail.com>
2023-05-06 22:21:46 +0430

    0.0.7 add Projects_page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,23 +1,45 @@
+import * as React from 'react';
+
 import image1 from '../../public/assets/images/food/carissa-gan-LdfLThHJB7c-unsplash.jpg';
 
 const projects = () => {
   const array = [
-    { title: 'All', id: 1 },
-    { title: '2', id: 2 },
-    { title: '3', id: 3 },
-    { title: '4', id: 4 },
-  ];
-  const array2 = [
-    // eslint-disable-next-line global-require
     {
-      title: 'project1',
-      // eslint-disable-next-line global-require
-      image: require('../../public/assets/images/food/carissa-gan-LdfLThHJB7c-unsplash.jpg')
-        .default,
+      title: 'All',
+      id: 1,
+      items: [
+        { title: 'project2', image: image1 },
+        { title: 'project2', image: image1 },
+      ],
     },
-    { title: 'project2', image: image1 },
-    { title: 'project3', image: image1 },
+    {
+      title: '2',
+      id: 2,
+      items: [
+        { title: 'project2', image: image1 },
+        { title: 'project2', image: image1 },
+      ],
+    },
+    {
+      title: '3',
+      id: 3,
+      items: [
+        { title: 'project2', image: image1 },
+        { title: 'project2', image: image1 },
+      ],
+    },
+    {
+      title: '4',
+      id: 4,
+      items: [
+        { title: 'project2', image: image1 },
+        { title: 'project2', image: image1 },
+      ],
+    },
   ];
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [index, setIndex] = React.useState('All');
 
   return (
     <div className="h-[1000px] p-[100px]">
@@ -25,6 +47,7 @@ const projects = () => {
         {array.map((item) => (
           <div
             key={item.id}
+            onClick={() => setIndex(item.title)}
             className="text-white cursor-pointer hover:bg-white/25 py-1 px-2 rounded	cursor-pointer transition-all"
           >
             {item.title}
@@ -32,12 +55,16 @@ const projects = () => {
         ))}
       </div>
       <div className="w-[75%] mx-auto justify-between flex">
-        {array2.map((item, index) => (
-          <div key={index} className="cursor-pointer relative w-[30%] h-[400px] overflow-hidden">
-            <img src={item.image} alt="image" className="object-cover w-full h-full" />
-            <div className="absolute left-7 bottom-8 text-3xl font-bold ">{item.title}</div>
-          </div>
-        ))}
+        {array.map((item) =>
+        {item.title === index ?
+          ( item.items.map((i,index)=>(
+              <div key={index} className="cursor-pointer relative w-[30%] h-[400px] overflow-hidden">
+                <img src={i.image} alt="image" className="object-cover w-full h-full" />
+                <div className="absolute left-7 bottom-8 text-3xl font-bold ">{i.title}</div>
+              </div>
+            ))): null
+        }
+        )}
       </div>
     </div>
   );
