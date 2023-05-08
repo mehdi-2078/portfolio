00c917d7ca1cb1bf81a2d5182a9242818e90b64d
faezehJafari <faezehj487@gmail.com>
2023-05-08 15:18:26 +0430

    0.0.8 edit

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,35 +1,41 @@
 export const Contact = () => {
   return (
-    <div className="w-[70%] md:w-[45%] mx-auto">
-      <div className="text-center mb-20">
-        <span className="text-yellow-400 text-3xl text-center pb-2 border-b-gray-200 border-b-2 ">
-          contact me
-        </span>
+    <div className="flex-col w-[80%] md:w-[80%] mx-auto flex md:flex-row ">
+      <div className="w-full md:w-[50%] mb-20 md:mb-0">
+        <div className="mb-10 text-center md:text-start">
+          <span className="text-yellow-400 text-3xl text-center pb-2 border-b-gray-200 border-b-2 ">
+            contact me
+          </span>
+        </div>
+        <p>jhgbhjbhbhjbhj</p>
       </div>
-      <div className="md:flex md:justify-between md:mb-4">
-        <input
+      <div className="w-full md:w-[50%]">
+        <div className="md:flex md:justify-between md:mb-4">
+          <input
+            placeholder="name"
+            className="mb-4 md:mb-0 w-full md:w-[49%] py-[14px] rounded-md pl-4 text-white outline-none bg-[#643A6B] text-xl"
+          />
+          <input
+            placeholder="name"
+            className="mb-4 md:mb-0 w-full md:w-[49%] py-[10px] rounded-md pl-4 text-white outline-none bg-[#643A6B] text-xl"
+          />
+        </div>
+        <div className="md:flex md:justify-between md:mb-4">
+          <input
+            placeholder="name"
+            className="mb-4 md:mb-0 w-full md:w-[49%] py-[14px] rounded-md pl-4 text-white outline-none bg-[#643A6B] text-xl"
+          />
+          <input
+            placeholder="name"
+            className="mb-4 md:mb-0 w-full md:w-[49%] py-[10px] rounded-md pl-4 text-white outline-none bg-[#643A6B] text-xl"
+          />
+        </div>
+        <textarea
           placeholder="name"
-          className="mb-4 md:mb-0 w-full md:w-[49%] py-[14px] rounded-md pl-4 text-white outline-none bg-[#643A6B] text-xl"
+          className="w-full rounded-md h-[150px] pl-4 text-white outline-none bg-[#643A6B] text-xl"
         />
-        <input
-          placeholder="name"
-          className="mb-4 md:mb-0 w-full md:w-[49%] py-[10px] rounded-md pl-4 text-white outline-none bg-[#643A6B] text-xl"
-        />
+        <button className="py-2 px-6 mt-2 rounded-md bg-white text-black">submit</button>
       </div>
-      <div className="md:flex md:justify-between md:mb-4">
-        <input
-          placeholder="name"
-          className="mb-4 md:mb-0 w-full md:w-[49%] py-[14px] rounded-md pl-4 text-white outline-none bg-[#643A6B] text-xl"
-        />
-        <input
-          placeholder="name"
-          className="mb-4 md:mb-0 w-full md:w-[49%] py-[10px] rounded-md pl-4 text-white outline-none bg-[#643A6B] text-xl"
-        />
-      </div>
-      <textarea
-        placeholder="name"
-        className="w-full rounded-md h-[150px] pl-4 text-white outline-none bg-[#643A6B] text-xl"
-      />
     </div>
   );
 };
