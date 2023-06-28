8df933d84d6bdbdeec645adefe433cec2e6d0687
mehdi <mehdizarei78@gmail.com>
2023-06-29 00:57:28 +0330

    0.0.20 edit

--- src/components/contact/ContactInfo.tsx
+++ src/components/contact/ContactInfo.tsx
@@ -1,6 +1,6 @@
 export const ContactInfo = () => {
   return (
-    <div className="w-full md:w-[40%] mr-auto mb-20 md:mb-0">
+    <div className="w-full md:w-[40%] mr-auto mb-10 md:mb-0">
       <div className="mb-10 text-center md:text-start">
         <span className="text-yellow-400 text-3xl text-center pb-2 border-b-gray-200 border-b-2 ">
           contact me
